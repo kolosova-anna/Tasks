a76b0455267a230c6887a8968755023b0b78aa1b
kolosova-anna <golikovaanna7365@mail.ru>
2022-10-16 00:30:06 +0200

    Add Task01 check square

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. На вход принимает два числа
+// 2. Проверяет, является ли первое число квадратом второго
+
+Console.WriteLine("Введите два целых числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}  
