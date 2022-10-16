2a9eb1457fc01b52a5ee3e086e0d71736390f7cb
kolosova-anna <golikovaanna7365@mail.ru>
2022-10-16 23:31:28 +0200

    Add Task o5 series of numbers

---
+++ Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая 
+// 1. на вход принимает одно число (N), а 
+// 2. на выход показывает все целые числа в промежутке от -N до N
+// 2 -> -2, -1, 0, 1, 2
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;
+    }
+}
+else Console.WriteLine("Введите целое положительное число"); 
