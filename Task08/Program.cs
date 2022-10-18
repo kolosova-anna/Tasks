c8edbedf9161f8786775d8ebed4cd7edac7ad1a1
kolosova-anna <golikovaanna7365@mail.ru>
2022-10-18 16:31:19 +0200

    Add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая
+// 1. на вход принимает число (N), а
+// 2. на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1)
+{
+    int count = 2;
+    while (count <= number)
+    {
+
+        if (count % 2 == 0)
+        {
+            Console.Write($"{count} ");
+        }
+        count++;
+    }
+}
+else Console.WriteLine("Четных положительных чисел не найдено");
