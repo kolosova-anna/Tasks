f8885429bab009fb4fcaa7eadc0795b5344dbc74
kolosova-anna <golikovaanna7365@mail.ru>
2022-10-20 00:01:04 +0200

    Add corrections

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -5,10 +5,32 @@
 // 46 -> нет
 // 161 -> да
 
-Console.WriteLine ("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int num1 = 7;
+// int num2 = 23;
 
-if (number % 7 == 0 && number % 23 == 0)
+// if (number % num1 == 0 && number % num2 == 0)
+// {
+//     Console.WriteLine ("да");
+// }
+// else Console.WriteLine ("нет");
+
+// Вариант 2
+bool DivNum(int digit, int num1, int num2)
+{
+    int result1 = digit % num1;
+    int result2 = digit % num2;
+    if (result1 == 0 && result2 == 0) return true;
+    return false;
+}
+Console.WriteLine ("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int number1 = 7;
+int number2 = 23;
+
+bool result = DivNum( num, number1, number2);
+if (result)
 {
     Console.WriteLine ("да");
 }
