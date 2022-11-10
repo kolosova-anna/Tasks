f3f289e54be72123b502f2630757d4e06e1f567a
kolosova-anna <golikovaanna7365@mail.ru>
2022-11-10 15:18:09 +0200

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая
+// задаёт массив из 8 элементов и
+// выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] NameArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-99, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+}
+
+int[] nameArray = NameArray(num);
+PrintArray(nameArray);
