9da1133198d869266d346d29011941ac49aebe25
kolosova-anna <golikovaanna7365@mail.ru>
2022-11-11 23:21:12 +0200

    Add Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу замены элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(8, -10, 10);
+PrintArray(arr);
+
+int[] ChangeSignNumberArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+int[] newArr = ChangeSignNumberArr(arr);
+PrintArray(newArr);
+
+
