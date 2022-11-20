63131b375875375b60f7f96831ff29428b7a8552
kolosova-anna <golikovaanna7365@mail.ru>
2022-11-21 00:10:51 +0200

    Correct Task48

--- Task48/Program.cs
+++ Task48/Program.cs
@@ -10,8 +10,12 @@ int[,] CreateMatrix(int rows, int columns)
 {
     int[,] matrix = new int[rows, columns];
     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
         for (int j = 0; j < matrix.GetLength(1); j++)
+        {
             matrix[i, j] = i + j;
+        }
+    }
     return matrix;
 }
 void PrintMatrix(int[,] matrix)
@@ -20,9 +24,11 @@ void PrintMatrix(int[,] matrix)
     {
         Console.Write("[");
         for (int j = 0; j < matrix.GetLength(1); j++)
+        {
             Console.Write("{0,4} ", matrix[i, j]);
 
-        Console.WriteLine("]");
+            Console.WriteLine("]");
+        }
     }
 }
 
