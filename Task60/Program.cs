63241791270e695d2a4f3dab82670448ba384288
kolosova-anna <golikovaanna7365@mail.ru>
2022-12-01 16:41:29 +0200

    Add Task64

---
+++ Task60/Program.cs
@@ -0,0 +1,67 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(min, max + 1);
+                int elem = matrix[i, j, k];
+                CheckMatrixNumbers(matrix, elem, min, max);
+                CheckMatrixNumbers(matrix, elem, min, max);
+
+            }
+
+        }
+    }
+    return matrix;
+}
+
+void CheckMatrixNumbers(int[,,] matrix, int element, int min, int max)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (element == matrix[i, j, k])
+                {
+                    matrix[i, j, k] = rnd.Next(min, max + 1);
+                }
+            }
+        }
+    }
+}
+
+void PrintMatrixWithIndex(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+
+int[,,] matrix3D = CreateMatrixRndInt(2, 2, 2, 10, 99);
+PrintMatrixWithIndex(matrix3D);
+
