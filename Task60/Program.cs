4f4e564e69b718ad020156aa43eaf4562ade89a1
kolosova-anna <golikovaanna7365@mail.ru>
2022-12-01 22:45:45 +0200

    Finnished Task60

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -8,7 +8,6 @@
 int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
 {
     int[,,] matrix = new int[rows, columns, depth];
-    Random rnd = new Random();
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -16,10 +15,29 @@ int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
-                matrix[i, j, k] = rnd.Next(min, max + 1);
-                int elem = matrix[i, j, k];
-                CheckMatrixNumbers(matrix, elem, min, max);
-                CheckMatrixNumbers(matrix, elem, min, max);
+                if (i == 0 && j == 0 && k == 0)
+                {
+                    matrix[i, j, k] = 10;
+                    continue;
+                }
+
+                if (k != 0)
+                {
+                    matrix[i, j, k] = matrix[i, j, k - 1] + 1;
+                    continue;
+                }
+
+                if (j != 0 && k == 0)
+                {
+                    matrix[i, j, k] = matrix[i, j - 1, matrix.GetLength(2) - 1] + 1;
+                    continue;
+                }
+
+                if (j == 0 && k == 0)
+                {
+                    matrix[i, j, k] = matrix[i - 1, matrix.GetLength(1) - 1, matrix.GetLength(2) - 1] + 1;
+                    continue;
+                }
 
             }
 
@@ -28,24 +46,6 @@ int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
     return matrix;
 }
 
-void CheckMatrixNumbers(int[,,] matrix, int element, int min, int max)
-{
-    Random rnd = new Random();
-
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int k = 0; k < matrix.GetLength(2); k++)
-            {
-                if (element == matrix[i, j, k])
-                {
-                    matrix[i, j, k] = rnd.Next(min, max + 1);
-                }
-            }
-        }
-    }
-}
 
 void PrintMatrixWithIndex(int[,,] matrix)
 {
@@ -61,7 +61,6 @@ void PrintMatrixWithIndex(int[,,] matrix)
     }
 }
 
-
-int[,,] matrix3D = CreateMatrixRndInt(2, 2, 2, 10, 99);
+int[,,] matrix3D = CreateMatrixRndInt(4, 4, 4, 10, 99);
 PrintMatrixWithIndex(matrix3D);
 
