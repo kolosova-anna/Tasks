e90eaa5350c58b061e1f964e8a29f953b38701ee
kolosova-anna <golikovaanna7365@mail.ru>
2022-12-02 00:49:31 +0200

    Add Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите натуральное число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int ProductNumbers(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    return numA * ProductNumbers(numA, numB - 1);
+}
+
+while (numberB < 0)
+{
+    Console.WriteLine("Некорректное значение");
+    Console.WriteLine("Введите натуральное число B: ");
+    numberB = Convert.ToInt32(Console.ReadLine());
+}
+
+int productNumbers = ProductNumbers(numberA, numberB);
+Console.WriteLine($"Число A в степени B = {productNumbers}");
